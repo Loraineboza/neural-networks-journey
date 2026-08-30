15da9af7029729a4da8e7ae3a8806ffca7ca8467
Loraineboza <pauldanilchenko2000@gmail.com>
2026-08-30 23:20:34 +0300

    fix graph

--- autograd/engine.py
+++ autograd/engine.py
@@ -6,41 +6,53 @@ class Tensor:
         self.grad = np.zeros_like(self.data) #матрица данных.shape == градиент.shape 
         self.op = op
         self.parents = parents
-        self.backward = lambda: None
+        self._backward = lambda: None
 
     def __add__(self, other):
         ret = Tensor(self.data + other.data, '+', self, other)
-        def _backward():
+        def backward():
             for parent in ret.parents:
                 parent.grad += ret.grad
-                parent.backward()
 
-        ret.backward = _backward
+        ret._backward = backward
         return ret
 
     def __mul__(self, other):
         ret = Tensor(self.data * other.data, '*', self, other)
-        def _backward():
+        def backward():
             for i in range(len(ret.parents)):
                 ret.parents[i].grad += ret.grad * ret.parents[0 if i>0 else 1].data
-                ret.parents[i].backward()
 
-        ret.backward = _backward
-        return ret 
-
-graph = []
-def build_graph(tensor): #постройка графа: поиск всех зависимых от <tensor> объектов, которые идут в последовательном неповторяющийся порядке
-    global graph
-    for i in tensor.parents:
-        if not(i in graph):
-            graph.append(i)
-        build_graph(i)
-    return graph
+        ret._backward = backward
+        return ret
     
+    def backward(self):
+        graph = build_graph(self)
+        self.grad = 1
+        for tensor in graph:
+            tensor._backward()
+def build_graph(tensor, graph=None): #постройка графа: поиск всех зависимых от <tensor> объектов, которые идут в последовательном неповторяющийся порядке
+    if graph is None:
+        graph = []
+
+    if tensor not in graph:
+        graph.append(tensor)
+
+    for parent in tensor.parents:
+        build_graph(parent, graph)
+
+    return graph
+
+
 a = Tensor(2)
 b = Tensor(3)
 
 c = a + b
-d = a * c
+d = c * a
 
-print(build_graph(d))
+d.backward()
+
+print(a.grad)
+print(b.grad)
+print(c.grad)
+print(d.grad)
