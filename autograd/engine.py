375d159c3456a60313911a8a1c455546f5ddee62
Loraineboza <pauldanilchenko2000@gmail.com>
2026-08-30 23:38:45 +0300

    added sub, pow

--- autograd/engine.py
+++ autograd/engine.py
@@ -26,33 +26,46 @@ class Tensor:
         ret._backward = backward
         return ret
     
+    def __sub__(self, other):
+        ret = Tensor(self.data - other.data, '-', self, other)
+        def backward():
+            for parent in ret.parents:
+                parent.grad += ret.grad -1 #производная от sub = -1 
+            ret._backward = backward 
+            return ret
+
+    def __pow__(self, other):
+        ret = Tensor(self.data ** other.data, '**', self, other)
+        def backward():
+            for i in range(len(ret.parents)):
+                ret.parents[i].grad += ret.grad * (ret.parents[i].data ** other.data)
+
     def backward(self):
         graph = build_graph(self)
         self.grad = 1
-        for tensor in graph:
+        for tensor in graph[::-1]: 
             tensor._backward()
+
 def build_graph(tensor, graph=None): #постройка графа: поиск всех зависимых от <tensor> объектов, которые идут в последовательном неповторяющийся порядке
     if graph is None:
         graph = []
 
     if tensor not in graph:
+        for parent in tensor.parents:
+            build_graph(parent, graph)
         graph.append(tensor)
 
-    for parent in tensor.parents:
-        build_graph(parent, graph)
-
     return graph
-
 
 a = Tensor(2)
 b = Tensor(3)
 
-c = a + b
-d = c * a
+c = a ** 2
+d = c - b
+e = d * a
 
-d.backward()
+e.backward()
 
-print(a.grad)
-print(b.grad)
 print(c.grad)
 print(d.grad)
+print(e.grad)
